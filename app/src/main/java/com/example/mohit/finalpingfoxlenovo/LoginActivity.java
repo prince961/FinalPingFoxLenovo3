b1786ede5b00007dc40622c6168ea1a2b63c98ca
Mohit <prince.prince961@gmail.com>
2018-11-13 11:38:21 +0530

    updated2

--- app/src/main/java/com/example/mohit/finalpingfoxlenovo/LoginActivity.java
+++ app/src/main/java/com/example/mohit/finalpingfoxlenovo/LoginActivity.java
@@ -1,14 +1,18 @@
 package com.example.mohit.finalpingfoxlenovo;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.method.LinkMovementMethod;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
@@ -33,11 +37,14 @@ public class LoginActivity extends AppCompatActivity {
 
     private FirebaseAuth mAuth;
     private final static int RC_SIGN_IN = 2;
-    GoogleSignInClient mGoogleSignInClient;private GoogleSignInAccount account;
+    GoogleSignInClient mGoogleSignInClient;
+    private GoogleSignInAccount account;
     private DatabaseReference databaseReference;
     FirebaseDatabase database;
     SharedPreferences sharedPreferences;
     SharedPreferences.Editor editor;
+    TextView termsPrivacy;
+    ProgressDialog progressDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,10 +58,18 @@ public class LoginActivity extends AppCompatActivity {
 
         SignInButton gButton;
         gButton = (SignInButton) findViewById(R.id.googleSignInBtn);
+        termsPrivacy = (TextView) findViewById(R.id.termsPrivacy);
+        termsPrivacy.setMovementMethod(LinkMovementMethod.getInstance());
         gButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Log.w("ClickListner", "button pressed");
+                progressDialog = new ProgressDialog(getApplicationContext());
+                progressDialog.setMessage("Searching for pingfox Device on your network");
+                progressDialog.setIndeterminate(false);
+                progressDialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+                progressDialog.setCancelable(true);
+                progressDialog.show();
                 Intent signInIntent = mGoogleSignInClient.getSignInIntent();
                 startActivityForResult(signInIntent, RC_SIGN_IN);
                 Log.w("ClickListner", "Data sent");
@@ -90,6 +105,13 @@ public class LoginActivity extends AppCompatActivity {
     private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
         try {
             account = completedTask.getResult(ApiException.class);
+            /*
+            ProgressDialog progressDialog = ProgressDialog.show(getBaseContext(),"Logging in","please wait");
+            progressDialog.setMessage("Searching for pingfox Device on your network");
+            progressDialog.setIndeterminate(false);
+            progressDialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+            progressDialog.setCancelable(true);
+            progressDialog.show(); */
             // Signed in successfully, show authenticated UI.
             //Create a user account on firebase with firebaseAuthWithGoogle method
             Log.i("Line Status","firebase auth with google method excecuted");
@@ -99,7 +121,7 @@ public class LoginActivity extends AppCompatActivity {
             // The ApiException status code indicates the detailed failure reason.
             // Please refer to the GoogleSignInStatusCodes class reference for more information.
             Log.w("TAG", "signInResult:failed code=" + e.getStatusCode());
-            //updateUI(null);
+            //updateUI(null);8976473163
         }
     }
 
@@ -113,6 +135,8 @@ public class LoginActivity extends AppCompatActivity {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if (task.isSuccessful()) {
+
+
                             // Sign in success, update UI with the signed-in user's information
                             Log.d("firebase auth", "signInWithCredential:success");
                             FirebaseUser firebaseUser = mAuth.getCurrentUser();
